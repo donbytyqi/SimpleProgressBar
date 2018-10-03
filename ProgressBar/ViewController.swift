637c7ac66ccbfd0c8871289d4891c79ad3604db7
Doni <donisign@yahoo.com>
2018-10-03 16:16:53 +0200

    Initial Commit

---
+++ ProgressBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProgressBar
+//
+//  Created by Don Bytyqi on 10/3/18.
+//  Copyright © 2018 Don Bytyqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
